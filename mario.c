51ac8be5cfac160ca069b7af1a4bf17eb7e253a2
Druzrka <zazman1999@gmail.com>
2017-02-08 21:36:06 +0300

    Update mario.c

--- mario.c
+++ mario.c
@@ -1,22 +1,28 @@
 #include <cs50.h>
 #include <stdio.h>
 
-int height;
 
 int main(void)
 {
-    printf("height:");
+    int height=0;
+    do{
+    printf("Height:");
     height = get_int();
+    }while(height<0);
+    
+    if (height >= 24) height = 2;
+    
     for (int i=1; i<height+1; i++)
-    {
+        {
         for(int j=0; j<height-i; j++)
-        {
-            printf(" ");
-        }
+            {
+                printf(" ");
+            }
         for(int k=0; k<i+1; k++)
-        {
-           printf("#"); 
-        }
+            {
+                printf("#"); 
+            }
         printf("\n");
-    }
+        }
+        
 }
